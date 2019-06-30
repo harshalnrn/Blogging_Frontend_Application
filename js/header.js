c2e81ec78d2efd3260fe4ac797bae191035aded4
Rajat0023 <rajatsingh2237@gmail.com>
2019-06-30 10:43:55 +0530

    Added Display Functionality for Sign In Modal using JS

---
+++ js/header.js
@@ -0,0 +1,17 @@
+/* Adding Display Functionality for the Sign-In Modal Box */
+
+var modal_sign_in = document.getElementById("SignInModal");
+var modal = document.getElementById("SignUpModal");
+
+function Sign_In() {
+    modal_sign_in.style.display = "block";
+}
+function span_SignIn() {
+    modal_sign_in.style.display = "none";
+}
+
+function Sign_Up_Sign_In() {
+    modal_sign_in.style.display = "none";
+    modal.style.display = "block";
+
+}
