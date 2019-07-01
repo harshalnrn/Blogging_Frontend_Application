4db929f3d65b4123f510c20af694433bc847a719
Rajat0023 <rajatsingh2237@gmail.com>
2019-07-01 19:19:31 +0530

    Added Styling for Delete modals, Made space-evenly for feature-B & Relevant comments

--- js/bloglist.js
+++ js/bloglist.js
@@ -1,14 +1,17 @@
 
 var modal_delete_blog = document.getElementById("DeleteBlogModal");
 
+/* Display the Delete Modal on clicking the Trash icon in each blog post */
 function deleteBlog() {
 modal_delete_blog.style.display="block";
 }
 
+/* When user Clicks on No button, close the Modal */
 function delete_modal_failure() {
     modal_delete_blog.style.display="none";
 }
 
+/* Redirect to post.html on clicking the (...) icon */
 function displayText(){
     window.location.href='../html/post.html';
 }
